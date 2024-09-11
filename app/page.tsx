b24ed75be06f0998af2891340b6f217e554ba5c2
[mobina7849] <[mobina.abbasi7849@gmail.com]>
2024-09-11 17:27:56 +0330

    fix(main): Removed extra url

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ import { ThemeProvider } from "@mui/material";
 import globalTheme from "../themes/globalTheme";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import ProjectTabs from "./components/ProjectTabs";
 import Project1 from "./components/Project1";
 
 export default function Home() {
